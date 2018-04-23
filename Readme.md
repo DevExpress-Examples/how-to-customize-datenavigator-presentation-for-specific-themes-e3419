0f253a359ce12cf5e5987b90f104ccff88443751
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize DateNavigator presentation for specific themes
+
+
+<p>To accomplish this task, override the DateNavigator ControlTemplates with <strong>x:Key</strong> attribute values defined in the following manner:</p><p><ControlTemplate x:Key="{dxet:DateNavigatorThemeKey ThemeName=<Target_theme_name>, ...</p><p>This example shows how to do this for <strong>LightGray</strong> and <strong>Office2007Blue</strong> themes. You can easily add templates for other themes by using the same approach.</p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Controls
+Imports System.Globalization
+
+Namespace SchedulerDateNavigatorCustomizationWpf
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+
+	Public Class ThemeNameConverterExtension
+		Inherits MarkupExtension
+		Implements IValueConverter
+		Private Shared instance As New ThemeNameConverterExtension()
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return instance
+		End Function
+
+		#Region "IValueConverter Members"
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+			Return value
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Dim listBoxItem As ListBoxItem = TryCast(value, ListBoxItem)
+
+			If listBoxItem IsNot Nothing Then
+				Return listBoxItem.Content
+			End If
+
+			Return value
+		End Function
+		#End Region
+	End Class
+End Namespace
